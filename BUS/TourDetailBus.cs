b8cdb13a3603507886f75d8e89027f64f082bd3a
rotkquanghien <hien124ytbg@gmail.com>
2020-09-01 17:13:48 +0700

    up product detail

---
+++ BUS/TourDetailBus.cs
@@ -0,0 +1,21 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class TourDetailBus
+    {
+        Model model = new Model();
+        public DataTable getTourDetail()
+        {
+            string sql = "";
+
+            return model.GetTable(sql);
+        }
+    }
+}
---
+++ DTO/PriceTourDetail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class PriceTourDetail
+    {
+        public string startDate { get; set; }
+        public string startTime { get; set; }
+        public List<PriceTour> prices { get; set; }
+    }
+}
