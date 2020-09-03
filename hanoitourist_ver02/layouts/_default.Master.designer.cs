91386cc0c23fc4c35778f00f425b68bf13b8e7cd
rotkquanghien <hien124ytbg@gmail.com>
2020-09-03 23:19:02 +0700

    "add count user visited"

--- hanoitourist_ver02/layouts/_default.Master.designer.cs
+++ hanoitourist_ver02/layouts/_default.Master.designer.cs
@@ -103,5 +103,14 @@ namespace hanoitourist_ver02.layouts
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label userOnline;
+
+        /// <summary>
+        /// userVisited control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label userVisited;
     }
 }
