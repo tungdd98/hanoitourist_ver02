2171ac928370fc5d78405198886d410fb131d4db
rotkquanghien <hien124ytbg@gmail.com>
2020-09-04 09:42:55 +0700

    add thong ke

--- hanoitourist_ver02/admin/dashboard/index.aspx.designer.cs
+++ hanoitourist_ver02/admin/dashboard/index.aspx.designer.cs
@@ -31,5 +31,23 @@ namespace hanoitourist_ver02.admin.dashboard
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label userOnline;
+
+        /// <summary>
+        /// tourName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label tourName;
+
+        /// <summary>
+        /// tourQuantity control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label tourQuantity;
     }
 }
